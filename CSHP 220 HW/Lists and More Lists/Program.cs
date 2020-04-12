70e16dd2642f5b390aa965106e0bd7b901565266
Kicka5h <kick.ash.smash@gmail.com>
2020-04-12 14:01:11 -0700

    assignment 2 in-progress
    console app using Linq query and no for/foreach

---
+++ CSHP 220 HW/Lists and More Lists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lists_and_More_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<User>();
+
+            users.Add(new User { Name = "Dave", Password = "hello" });
+            users.Add(new User { Name = "Steve", Password = "steve" });
+            users.Add(new User { Name = "Lisa", Password = "hello" });
+
+            static bool isHello(User pass)
+            {
+                if (pass.Password == "hello")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            List<User> passResults = users.FindAll(isHello);
+            if (passResults.Count != 0)
+            {
+                DisplayResults(passResults, "Password is hello:");
+            }
+
+            static void DisplayResults(List<User> results, string userResults)
+            {
+                Console.WriteLine();
+                Console.WriteLine(userResults);
+                foreach (User b in results)
+                {
+
+                    Console.Write("\n{0}\t{1}", b.Name,
+                        b.Password);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
