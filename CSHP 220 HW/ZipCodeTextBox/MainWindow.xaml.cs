131cc8a561d8a9beec61417176b7c181b9ab527b
Kicka5h <karczag.ashley@gmail.com>
2020-04-26 21:48:12 -0700

    homework

--- CSHP 220 HW/ZipCodeTextBox/MainWindow.xaml.cs
+++ CSHP 220 HW/ZipCodeTextBox/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ZipCodeTextBox.Models;
 
 namespace ZipCodeTextBox
 {
@@ -21,40 +21,63 @@ namespace ZipCodeTextBox
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Models.ZipCode zipcode = new Models.ZipCode();
-
         public MainWindow()
         {
             InitializeComponent();
 
-            uxContainer.DataContext = zipcode;
+            DataContext = uxContainer;
+        }
+
+        private bool IsUSOrCanadianZipCode(string zipCode)
+        {
+            var _usZipRegEx = @"^\d{5}(?:[-\s]\d{4})?$";
+            var _caZipRegEx = @"^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$";
+
+            var validZipCode = true;
+            if ((!Regex.Match(zipCode, _usZipRegEx).Success) && (!Regex.Match(zipCode, _caZipRegEx).Success))
+            {
+                validZipCode = false;
+            }
+            return validZipCode;
         }
 
         private void setButtonEnable()
         {
-            if ((uxUSZip.Text == String.Empty) && (uxCANZip.Text == String.Empty)) 
+            if (IsUSOrCanadianZipCode(uxZip.Text) != false)
             {
-                uxButton.IsEnabled = false;
+                uxSubmit.IsEnabled = true;
             }
-            else 
+            else
             {
-                uxButton.IsEnabled = true;
+                uxSubmit.IsEnabled = false;
             }
         }
 
-        private void uxUSZip_TextChanged(object sender, TextChangedEventArgs e)
+        private void setLabelText()
+        {
+            if ((IsUSOrCanadianZipCode(uxZip.Text) != true))
+            {
+                uxZipError.Foreground = Brushes.Red;
+                uxZipError.Content = "Please enter a valid US or CAN zip code.";
+            }
+            else
+            {
+                uxZipError.Content = "";
+            }
+        }
+
+        public void uxZip_TextChanged(object sender, TextChangedEventArgs e)
         {
             setButtonEnable();
+            setLabelText();
         }
 
-        private void uxCANZip_TextChanged(object sender, TextChangedEventArgs e)
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
         {
-            setButtonEnable();
-        }
-
-        private void uxButton_Click(object sender, RoutedEventArgs e)
-        {
-            
+            if (IsUSOrCanadianZipCode(uxZip.Text) != false)
+            {
+                MessageBox.Show("Thanks, your zip code has been submitted.");
+            }
         }
     }
 }
