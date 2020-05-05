15fd60d0dee1f0a4691d99f612f1c703b4a90f11
Kicka5h <kick.ash.smash@gmail.com>
2020-05-04 20:35:42 -0700

    Everything short of a win condition.

--- CSHP 220 HW/TicTacToe/MainWindow.xaml.cs
+++ CSHP 220 HW/TicTacToe/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        int clicks;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,17 +29,36 @@ namespace TicTacToe
 
         private void uxNewGame_Click(object sender, RoutedEventArgs e)
         {
-
+            clicks = 0;
+            
+            foreach (DependencyObject cell in uxGrid.Children)
+            {
+                ((Button)cell).Content = String.Empty;
+            }
         }
 
         private void uxExit_Click(object sender, RoutedEventArgs e)
         {
-
+            //this.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            clicks++;
 
+            Button button = e.Source as Button;
+
+            var ParseTag = button.Tag.ToString().Split(",");
+
+            int GridRow = int.Parse(ParseTag[0]);
+            int GridColumn = int.Parse(ParseTag[1]);
+
+            if (clicks%2 == 0)
+            {
+                button.Content = "O";    
+            }
+            else {button.Content = "X";}
         }
+
     }
 }
