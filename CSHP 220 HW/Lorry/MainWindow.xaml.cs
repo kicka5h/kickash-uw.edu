30c6b890265158fa395bfd67cbf16b24d2fe24a1
Kicka5h <karczag.ashley@gmail.com>
2020-05-18 21:10:52 -0700

    lorry updates

--- CSHP 220 HW/Lorry/MainWindow.xaml.cs
+++ CSHP 220 HW/Lorry/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace Lorry
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Events
     {
         public MainWindow()
         {
@@ -17,78 +17,6 @@ namespace Lorry
             uxRecentPoem.DataContext = recentPoem.Recent.Local.ToObservableCollection();
         }
 
-        private void uxFile_Loaded(object sender, RoutedEventArgs e)
-        {
 
-        }
-
-        private void uxFileNew_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxFileNewHaiku_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        public void uxFileNewCouplet_Click(object sender, RoutedEventArgs e)
-        {
-            var coupletWindow = new CoupletWindow();
-            var mainWindow = new MainWindow();
-            Application.Current.MainWindow = coupletWindow;
-            mainWindow.Close();
-            coupletWindow.Show();
-        }
-
-        private void uxFileOpen_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxFileExit_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxEdit_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxEditAdd_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxEditAddHaiku_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxEditAddCouplet_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxHelp_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxEditAddCoupletRhyme_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxFileNewFreeForm_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void uxRecentLabel_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
