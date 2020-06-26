df815673edb5b00e96bb4e0d2793479450c54b34
Kicka5h <karczag.ashley@gmail.com>
2020-06-25 18:55:37 -0700

    New ASP.NET project

---
+++ CSHP 230/HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
