9df05fc85c933c3fb2d209854a30beb8087bc73f
Kicka5h <karczag.ashley@gmail.com>
2020-08-23 11:53:35 -0700

    Initial commit

---
+++ CSHP 230/RestService/RestServiceClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RestServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:#####/api/");
+
+            var result = client.GetAsync("users").Result;
+
+            var json = result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(json);
+
+            var list = JsonConvert.DeserializeObject<List<dynamic>>(json);
+            Console.ReadLine();
+        }
+    }
+}
